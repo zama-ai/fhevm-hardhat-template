320552bf41cfa60bc46bb4bd9be17c8fe0d6fcb2
Aurora Poppyseed <30662672+poppyseedDev@users.noreply.github.com>
2025-08-25 15:35:35 +0200

    fix: update hardhat.config.ts version
    When working with OZ's contracts this is a necessary fix that you need to do to make it work.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -65,7 +65,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.24",
+    version: "0.8.27",
     settings: {
       metadata: {
         // Not including the metadata hash
