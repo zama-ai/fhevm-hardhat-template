295bb923c78e1c151dbd42a9edc995b1fcdf4f50
0xalexbel <alexandre.belhoste@zama.ai>
2025-11-13 10:27:59 +0100

    chore: fhevm v0.9.1

--- contracts/FHECounter.sol
+++ contracts/FHECounter.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.24;
 
 import {FHE, euint32, externalEuint32} from "@fhevm/solidity/lib/FHE.sol";
-import {EthereumConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
+import {ZamaEthereumConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 
 /// @title A simple FHE counter contract
 /// @author fhevm-hardhat-template
 /// @notice A very basic example contract showing how to work with encrypted data using FHEVM.
-contract FHECounter is EthereumConfig {
+contract FHECounter is ZamaEthereumConfig {
     euint32 private _count;
 
     /// @notice Returns the current count
